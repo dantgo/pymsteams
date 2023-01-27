448f553a208f1040973b2302fdfdca6593da7d4c
Daniel Gore <47426748+dantgo@users.noreply.github.com>
2023-01-27 09:20:40 +0000

    Update __init__.py

--- pymsteams/__init__.py
+++ pymsteams/__init__.py
@@ -232,22 +232,22 @@ class connectorcard:
     @job('high')
     def send(self):
         pass
-    send.delay()
-        headers = {"Content-Type": "application/json"}
-        r = requests.post(
-            self.hookurl,
-            json=self.payload,
-            headers=headers,
-            proxies=self.proxies,
-            timeout=self.http_timeout,
-            verify=self.verify,
-        )
-        self.last_http_response = r
+        send.delay()
+            headers = {"Content-Type": "application/json"}
+            r = requests.post(
+                self.hookurl,
+                json=self.payload,
+                headers=headers,
+                proxies=self.proxies,
+                timeout=self.http_timeout,
+                verify=self.verify,
+            )
+            self.last_http_response = r
 
-        if r.status_code == requests.codes.ok:  # pylint: disable=no-member
-            return True
-        else:
-            raise TeamsWebhookException(r.text)
+            if r.status_code == requests.codes.ok:  # pylint: disable=no-member
+                return True
+            else:
+                raise TeamsWebhookException(r.text)
 
     def __init__(self, hookurl, http_proxy=None, https_proxy=None, http_timeout=60, verify=None):
         self.payload = {}
